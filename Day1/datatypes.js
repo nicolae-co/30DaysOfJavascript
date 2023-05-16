1632e0f38c3c5381ddab9ee90f7779e4122454e5
nicolae-co <cojocarugithub@gmail.com>
2023-05-16 22:48:17 +0300

    30DaysOfJS 1

---
+++ Day1/datatypes.js
@@ -0,0 +1,5 @@
+// 5.
+console.log(typeof stringVar);
+console.log(typeof booleanVar);
+console.log(typeof undefinedVar);
+console.log(typeof nullVar);
---
+++ Day1/variable.js
@@ -0,0 +1,37 @@
+// 4.
+const stringVar = 'This is a string';
+
+const booleanVar = true;
+
+let undefinedVar;
+
+let nullVar = null;
+
+// 6.
+let firstVar, secondVar, thirdVar, forthVar;
+
+// 7.
+const varOne = 23;
+const varTwo = 'string';
+const varTree = 34.23;
+const varFour = 'This is a string';
+
+// 8.
+const firstName2 = 'Nicolae';
+const secondName2 = 'Cojocaru';
+const maritalStatus2 = 'single';
+const country2 = 'Romania';
+const age2 = 27;
+
+// 9.
+let firstName = 'Nicolae',
+  secondName = 'Cojocaru',
+  maritalStatus = 'single',
+  country = 'Romania',
+  age = 27;
+
+// 10.
+const myAge = 27;
+const yourAge = 34;
+
+console.log(myAge, yourAge);
